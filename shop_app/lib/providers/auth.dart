8c605319801da5ef256822e8b955f5cb8a24f0ad
swapyinc <swapyinc@gmail.com>
2021-09-25 01:55:26 +0200

    finish authentication

--- shop_app/lib/providers/auth.dart
+++ shop_app/lib/providers/auth.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/widgets.dart';
@@ -8,6 +9,7 @@ class Auth with ChangeNotifier {
   String? _token;
   DateTime? _expiryDate;
   String? _userId;
+  Timer? _autoLogoutTimer = null;
 
   bool isAuthenticated() {
     return token != null;
@@ -52,6 +54,7 @@ class Auth with ChangeNotifier {
       _expiryDate = DateTime.now()
           .add(Duration(seconds: int.parse(responseBody['expiresIn'])));
 
+      _autoLogout();
       notifyListeners();
     } catch (error) {
       throw error;
@@ -64,5 +67,23 @@ class Auth with ChangeNotifier {
 
   Future<void> login(String email, String password) async {
     return _authenticate(email, password, 'verifyPassword');
+  }
+
+  void logout() {
+    _token = null;
+    _expiryDate = null;
+    _userId = null;
+    _autoLogoutTimer!.cancel();
+    _autoLogoutTimer = null;
+    notifyListeners();
+  }
+
+  void _autoLogout() {
+    if (_autoLogoutTimer != null) {
+      _autoLogoutTimer!.cancel();
+      _autoLogoutTimer = null;
+    }
+    final diff = _expiryDate!.difference(DateTime.now()).inSeconds;
+    _autoLogoutTimer = Timer(Duration(seconds: diff), logout);
   }
 }
--- shop_app/lib/widgets/app_drawer.dart
+++ shop_app/lib/widgets/app_drawer.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../screens/orders_screen.dart';
 import '../screens/user_products_screen.dart';
+import '../providers/auth.dart';
 
 class AppDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final authProvider = Provider.of<Auth>(context, listen: false);
+
     return Drawer(
       child: Column(
         children: [
@@ -33,6 +37,15 @@ class AppDrawer extends StatelessWidget {
             onTap: () => Navigator.of(context)
                 .pushReplacementNamed(UserProductsScreen.routeName),
           ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.logout),
+            title: Text("Logout"),
+            onTap: () {
+              Navigator.of(context).popAndPushNamed("/");
+              authProvider.logout();
+            },
+          )
         ],
       ),
     );
